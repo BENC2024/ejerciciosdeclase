309a5388bfd6067064d0e06e459423fc8377ec9f
BENC2024 <brahiangithub@gmail.com>
2023-05-29 14:01:12 -0500

    modificado Angulo

--- Ejercicio_2/angulo.py
+++ Ejercicio_2/angulo.py
@@ -1,12 +1,15 @@
 
-angulo = float(input("\nIngrese angulo: "))
+try:
+    angulo = float(input("\nIngrese angulo: "))
 
-if angulo >= 0:
-    if angulo < 90:
-        print("\nEl angulo de",angulo,"grados es un angulo agudo\n")
-    elif angulo == 90:
-        print("\nEl angulo de",angulo,"grados es un angulo recto\n")
+    if angulo >= 0:
+        if angulo < 90:
+            print("\nEl angulo de",angulo,"grados es un angulo agudo\n")
+        elif angulo == 90:
+            print("\nEl angulo de",angulo,"grados es un angulo recto\n")
+        else:
+            print("\nEl angulo de",angulo,"grados es un angulo obtuso\n")
     else:
-        print("\nEl angulo de",angulo,"grados es un angulo obtuso\n")
-else:
-    print("\nNo se admiten angulos negativos\n")
+        print("\nNo se admiten angulos negativos\n")
+except:
+    print("\nHas ingresado un dato erroneo\n")
