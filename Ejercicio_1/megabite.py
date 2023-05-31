858c3b985f2082cb55302cc3a218340d7b9ea9b0
BENC2024 <brahiangithub@gmail.com>
2023-05-31 14:46:43 -0500

    Modificando megabites

--- Ejercicio_1/megabite.py
+++ Ejercicio_1/megabite.py
@@ -1,10 +1,10 @@
 
 try:
-    mega = float(input("ingrese los datos en megabyte: "))
+    mega = float(input("\ningrese los datos en megabyte: "))
 
-    print("el resultado en bits es: {}".format(int(mega*1024*1024*8)))
-    print("el resultado en bytes es: {}".format(int(mega*1024*1024)))
-    print("el resultado en kilobytes es: {}".format(int(mega*1024)))
-    print("el resultado en gigabytes es: {}".format(mega/1024))
+    print("\nEl resultado en bits es: {}".format(int(mega*1024*1024*8)))
+    print("El resultado en bytes es: {}".format(int(mega*1024*1024)))
+    print("El resultado en kilobytes es: {}".format(int(mega*1024)))
+    print("El resultado en gigabytes es: {}\n".format(mega/1024))
 except:
     print("\nEl dato ingresado es erroneo\n")
