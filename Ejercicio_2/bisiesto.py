613760e374ef71ee3e3389bea828c44d39f9f43b
BENC2024 <brahiangithub@gmail.com>
2023-06-06 15:35:32 -0500

    modificar bisiesto

--- Ejercicio_2/bisiesto.py
+++ Ejercicio_2/bisiesto.py
@@ -1,10 +1,15 @@
+#Bisiesto
+'''
+Se da año nuevo todos aquello que sean multiplos de 4
+Se excluyen los que son multiplos de 100 y 400
+'''
 
 try:
-    anio = int(input("\nIngrese el año: "))
+    anio = int(input("\nIngrese el año para saber si es un año bisiesto: "))
     if anio > 0:
         if anio >= 1582:
-            if anio % 4 == 0:
-                print("\nEl año {} es un año bisiesto\n".format(anio))
+            if anio % 4 == 0 and anio % 100 != 0 and anio % 400 != 0:
+                    print("\nEl año {} es un año bisiesto\n".format(anio))
             else:
                 print("\nEl año {} no es un año bisiesto\n".format(anio))
         else:
