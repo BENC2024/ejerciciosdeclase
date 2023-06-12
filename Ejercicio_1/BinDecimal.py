bdde41e71257bb6ea312d7bded5000c0bb427b5b
BENC2024 <brahiangithub@gmail.com>
2023-06-12 16:34:11 -0500

    Modificando bindecimal.py

--- Ejercicio_1/BinDecimal.py
+++ Ejercicio_1/BinDecimal.py
@@ -4,15 +4,27 @@ ingresado por teclado a numero  decimal, al final  deberá mostrar
 cual es su equivalencia
 '''
 
-binario = int(input("\nIngrese numero binario: "))
-div = binario
-i = 0
-decimal = 0
+try:
+    binario = int(input("\nIngrese numero binario: "))
+    b = binario
+    i = 0
+    decimal = 0
 
-while div > 0:
-    decimal = decimal + (div%10)*(2**i)
-    div = int(div/10)
-    i += 1
+    if binario >= 0:
+        while binario > 0:
+            if binario%10 <= 1:
+                decimal = decimal + (binario%10)*(2**i)
+                binario = int(binario/10)
+                i += 1
+            else:
+                break
 
-print("\n{} en numero decimal es {}\n".format(binario,decimal))
+        if binario == 0:
+            print("\n{} en numero decimal es {}\n".format(b,decimal))
+        else:
+            print("\nSolo se admiten numeros en binario\n")
+    else:
+        print("\nNo se admite signo negativo\n")
 
+except:
+    print("\nHas ingresado un dato erroneo\n")
