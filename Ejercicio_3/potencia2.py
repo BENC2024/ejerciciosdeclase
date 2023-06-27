3c3d940e95cefc3cd9823b517b67c066c6854892
BENC2024 <brahiangithub@gmail.com>
2023-06-27 11:38:44 -0500

    corrigiendo potencia2

--- Ejercicio_3/potencia2.py
+++ Ejercicio_3/potencia2.py
@@ -30,22 +30,23 @@ def suma_potencia(num1,num2):
     return msj
 
 try:
-     
-    numero = int(input("\nIngrese el numero: "))
-
-    if numero > 0:
-        if es_potencia_de_dos(numero) == True:
-            print("\nEl numero {} es potencia de 2".format(numero))
-        elif es_potencia_de_dos(numero) == False:
-            print("\nEl numero {} no es potencia de 2".format(numero))
-    else:
-        print("\nSolo se permiten numeros naturales (mayores a cero)")
     
     print("\nSuma de potencias de 2 (dos numeros mayores que cero):")
     num1 = int(input("Ingrese el primer numero: "))
     num2 = int(input("Ingrese el segundo numero: "))
-    
+
     if num1 > 0 and num2 > 0:
+
+        if es_potencia_de_dos(num1) == True:
+            print("\nEl numero {} es potencia de 2".format(num1))
+        elif es_potencia_de_dos(num1) == False:
+            print("\nEl numero {} no es potencia de 2".format(num1))
+
+        if es_potencia_de_dos(num2) == True:
+            print("El numero {} es potencia de 2".format(num2))
+        elif es_potencia_de_dos(num2) == False:
+            print("El numero {} no es potencia de 2".format(num2))
+
         print(f"\nLa suma de las potencias de 2 entre {num1} y {num2} es:")
         if num1 <= num2:
             print(suma_potencia(num1,num2))
